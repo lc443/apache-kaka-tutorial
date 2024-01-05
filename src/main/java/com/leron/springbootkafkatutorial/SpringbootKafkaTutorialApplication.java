9dea48ccf812d273caf166d6841861d0724e3800
Leron Charles <lc443@njit.edu>
2024-01-04 23:54:01 -0500

    set up kafka properties

---
+++ src/main/java/com/leron/springbootkafkatutorial/SpringbootKafkaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.leron.springbootkafkatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leron/springbootkafkatutorial/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leron.springbootkafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
