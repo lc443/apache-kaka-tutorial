f651d2a4cc9afc4ac7263c3e13df8d6c121556d5
Leron Charles <lc443@njit.edu>
2024-01-05 00:08:06 -0500

    implemented producer class and send message method

---
+++ src/main/java/com/leron/springbootkafkatutorial/kafka/KafkaProducer.java
@@ -0,0 +1,24 @@
+package com.leron.springbootkafkatutorial.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private static  final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        LOGGER.info(String.format("Message sent %s", message));
+        kafkaTemplate.send("leron", message);
+    }
+}
